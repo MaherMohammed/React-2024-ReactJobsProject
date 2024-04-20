1c5aefbb94fea697d46d40c98100f99ff7477e6a
MaherMohammed <maher.mohammed10.97@gmail.com>
2024-04-20 10:33:00 +0200

    adding the hero component

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,10 +1,16 @@
 import React from 'react'
+import logo from "../assets/images/logo.png"
 
 function Navbar() {
   return (
     <nav className='bg-indigo-700 border-b border-indigo-500'>
-    
-  </nav>
+        <img 
+        className='h-10 w-auto'
+            src={ logo }
+            alt='React-Jobs'
+        />
+        
+    </nav>
   )
 }
 
