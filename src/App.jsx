096ae50e9aeb650f6a2653b4c1da80e009933f3f
MaherMohammed <maher.mohammed10.97@gmail.com>
2024-04-20 10:24:25 +0200

    adding the navbar and logo

--- src/App.jsx
+++ src/App.jsx
@@ -1,24 +1,11 @@
 import React from 'react'
+import Navbar from './components/Navbar'
+
 
 const App = () => {
-  const name = 15;
-  const names = ['maher', 'mohammed','mostafa']
-  const loggedIn = false;
   return (
-    <div>
-      <div className='text-5xl'>
-        App
-      </div>
-      <p>Hi {name}</p>
-      <ul>
-        {names.map((name,index)=> (
-          <li key={index}>{name}</li>
-        ))}
-      </ul>
-      {loggedIn ? <h1>Hello User</h1> : <h1>Hello Guest</h1>}
-    </div>
+   <Navbar />
   )
 }
 
 export default App
- 
---
+++ src/components/Navbar.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+function Navbar() {
+  return (
+    <nav className='bg-indigo-700 border-b border-indigo-500'>
+    
+  </nav>
+  )
+}
+
+export default Navbar
