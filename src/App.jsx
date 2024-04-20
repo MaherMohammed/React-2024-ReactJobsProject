d9eb28aeca0629b100b11ee9822b090a6acbb537
MaherMohammed <maher.mohammed10.97@gmail.com>
2024-04-20 15:29:06 +0200

    adding the view all jobs button

--- src/App.jsx
+++ src/App.jsx
@@ -3,6 +3,7 @@ import Navbar from './components/Navbar'
 import Hero from './components/Hero'
 import HomeCards from './components/HomeCards'
 import JobListing from './components/JobListings'
+import ViewAllJobs from './components/ViewAllJobs'
 
 const App = () => {
   return (
@@ -11,6 +12,7 @@ const App = () => {
     <Hero />
     <HomeCards />
     <JobListing />
+    <ViewAllJobs />
    </>
   )
 }
