beb82911d74ef4c9ec045d1a8c4a0f8c40539d91
MaherMohammed <maher.mohammed10.97@gmail.com>
2024-04-20 11:42:08 +0200

    job listings recovery

--- src/components/JobListings.jsx
+++ src/components/JobListings.jsx
@@ -4,14 +4,14 @@ import JobListing from './JobListing'
 
 const JobListings = () => {
   return (
-    <section class="bg-blue-50 px-4 py-10">
-      <div class="container-xl lg:container m-auto">
-        <h2 class="text-3xl font-bold text-indigo-500 mb-6 text-center">
+    <section className="bg-blue-50 px-4 py-10">
+      <div className="container-xl lg:container m-auto">
+        <h2 className="text-3xl font-bold text-indigo-500 mb-6 text-center">
           Browse Jobs
         </h2>
-        <div class="grid grid-cols-1 md:grid-cols-3 gap-6">
+        <div className="grid grid-cols-1 md:grid-cols-3 gap-6">
             {jobs.map((job) => (
-                <JobListing job = {job}/>
+                <JobListing key={job.id} job = {job}/>
             ))}
           
         </div>
